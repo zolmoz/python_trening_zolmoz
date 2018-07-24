51706ecd0601b51465af728ac3af5cbb964cae1b
zolmo <mellamoryblim@gmail.com>
2018-07-24 15:00:13 +0800

    после неудачного запустка

--- test_ad_group.py
+++ test_ad_group.py
@@ -12,7 +12,7 @@ def is_alert_present(wd):
 
 class test_ad_group(unittest.TestCase):
     def setUp(self):
-        self.wd = WebDriver()
+        self.wd = WebDriver(capabilities={"marionette": False})
         self.wd.implicitly_wait(60)
     
     def test_test_ad_group(self):
