e0e7c8705423a2bc62b89546ecf46243a8145512
zolmo <mellamoryblim@gmail.com>
2018-07-28 21:39:24 +0800

    добавлен тест на удаление группы с кирилическим наименованием

--- fixture/groupdelete.py
+++ fixture/groupdelete.py
@@ -14,6 +14,15 @@ class GroupDelete:
         wd.find_element_by_name("delete").click()
         GroupHelper.return_to_groups_page(self)
 
+    def delete_cirilic_groupe (self):
+        wd = self.app.wd
+        GroupHelper.open_group_page(self)
+        #selest first groupe
+        wd.find_element_by_css_selector("[title^='Select (группа3)']").click()
+        #submit deletion
+        wd.find_element_by_name("delete").click()
+        GroupHelper.return_to_groups_page(self)
+
 
     def delete_all_groupe(self):
         wd = self.app.wd
--- test/test_fdel_groupe.py
+++ test/test_fdel_groupe.py
@@ -5,8 +5,14 @@ def test_delete_first_groupe_01(app):
     app.groupdelete.delete_first_groupe()
     app.session.logout()
 
+def test_delete_cirilic_groupe_02(app):
+    success = True
+    app.session.login(user_name="admin", password="secret")
+    app.groupdelete.delete_cirilic_groupe()
+    app.session.logout()
 
-def test_delete_all_groupe_02(app):
+
+def test_delete_all_groupe_03(app):
     success = True
     app.session.login(user_name="admin", password="secret")
     app.groupdelete.delete_all_groupe()
