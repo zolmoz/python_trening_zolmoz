9249a71eef3a901a3d23a3d92e63d8833621b231
zolmo <mellamoryblim@gmail.com>
2018-09-22 23:53:06 +0800

    Промежуточный перед загрузкой тестов

---
+++ data/groups.py
@@ -0,0 +1,7 @@
+from model.groupe import Groupe
+
+constast = [
+    Groupe(name="n1", header="h1", footer="f3"),
+    Groupe(name="n2", header="h2", footer="f2")
+]
+
--- test/test_ad_group.py
+++ test/test_ad_group.py
@@ -1,27 +1,12 @@
 # -*- coding: utf-8 -*-
 from model.groupe import Groupe
-import pytest
-import random
-import string
-
-def random_string(prefix,maxlen):
-    symbol = string.ascii_letters+string.digits + string.punctuation + " "*10
-    return prefix + "".join([random.choice(symbol) for i in range(random.randrange(maxlen))])
 
 
 
-testdata = [Groupe(name="", header="", footer="")] + [Groupe(name="группа3", header="группа3", footer="группа3")]+ [
-    Groupe(name=random_string("name", 10),
-          header=random_string("header", 20),
-          footer=random_string("footer", 20))
-    for i in range(5)
-   ]
 
 
-@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
-def test_test_ad_group(app, group):
-    success = True
-    pass
+def test_test_ad_group(app, data_groups):
+    group = data_groups
     old_groups = app.group.get_group_list()
     app.group.create(group)
     assert len(old_groups) + 1 == app.group.count()
