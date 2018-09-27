877ae263bd3d7cb4639dd84c65cc9192dd741997
zolmo <mellamoryblim@gmail.com>
2018-09-28 00:05:08 +0800

    Поправлен вызов теста внутри теста

--- test/test_delete_contact_from_groupe.py
+++ test/test_delete_contact_from_groupe.py
@@ -5,7 +5,7 @@ from test import test_ad_contact_to_groupe
 def test_delete_contact_from_group(app, db, orm):
     groups = orm.get_groups_with_contacts()
     if len(groups) == 0:
-        test_ad_contact_to_groupe()
+        test_ad_contact_to_groupe.test_add_contact_to_group(app, db, orm)
     group = random.choice(groups)
     contacts = orm.get_contacts_in_group(group)
     contact = random.choice(contacts)
