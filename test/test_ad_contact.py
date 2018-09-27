cbe0af9f8572d1fd1c6cded1ffd79761db9b4e24
zolmo <mellamoryblim@gmail.com>
2018-09-28 02:07:20 +0800

    добалвены шаги на создани контакта, и генератор отчетев alure, так же выполнена интеграция allure с jenkins

--- test/test_ad_contact.py
+++ test/test_ad_contact.py
@@ -1,18 +1,22 @@
 # -*- coding: utf-8 -*-
 from model.contactfilld import Contactfilld
+import  pytest
 
 
 
 def test_ad_contact_01(app, db, json_contact,check_ui):
     success = True
     contact = json_contact
-    old_contacts = db.get_contact_list()
-    app.contacts.newcontact(contact)
-    new_contacts = db.get_contact_list()
-    old_contacts.append(contact)
-    assert sorted(old_contacts, key=Contactfilld.id_or_max) == sorted(new_contacts, key=Contactfilld.id_or_max)
-    if check_ui:
-        assert sorted(new_contacts, key=Contactfilld.id_or_max()) == sorted(app.contacts.get_contact_list(), key=Contactfilld.id_or_max())
+    with pytest.allure.step('Given a contact list'):
+        old_contacts = db.get_contact_list()
+    with pytest.allure.step('when i add contact %s to the list' % contact):
+        app.contacts.newcontact(contact)
+    with pytest.allure.step('Then the new contact list is equal to the list with the added contact'):
+        new_contacts = db.get_contact_list()
+        old_contacts.append(contact)
+        assert sorted(old_contacts, key=Contactfilld.id_or_max) == sorted(new_contacts, key=Contactfilld.id_or_max)
+        if check_ui:
+            assert sorted(new_contacts, key=Contactfilld.id_or_max()) == sorted(app.contacts.get_contact_list(), key=Contactfilld.id_or_max())
 
 
 
--- test/test_ad_group.py
+++ test/test_ad_group.py
@@ -1,16 +1,20 @@
 # -*- coding: utf-8 -*-
 from model.groupe import Groupe
+import  pytest
 
 
 def test_test_ad_group(app, db, json_groups,check_ui):
     group = json_groups
-    old_groups = db.get_group_list()
-    app.group.create(group)
-    new_groups = db.get_group_list()
-    old_groups.append(group)
-    assert sorted(old_groups, key=Groupe.id_or_max) == sorted(new_groups, key=Groupe.id_or_max)
-    if check_ui:
-        assert sorted(new_groups, key=Groupe.id_or_max) == sorted(app.group.get_group_list(), key=Groupe.id_or_max)
+    with pytest.allure.step('Given a groupe list'):
+        old_groups = db.get_group_list()
+    with pytest.allure.step('when i add groupe %s to the list' % group):
+        app.group.create(group)
+    with pytest.allure.step('Then the new groupe list is equal to the list with the added groupe'):
+        new_groups = db.get_group_list()
+        old_groups.append(group)
+        assert sorted(old_groups, key=Groupe.id_or_max) == sorted(new_groups, key=Groupe.id_or_max)
+        if check_ui:
+            assert sorted(new_groups, key=Groupe.id_or_max) == sorted(app.group.get_group_list(), key=Groupe.id_or_max)
 
 
 
